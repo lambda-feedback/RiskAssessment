b6d7bb8c123275cdbcd7b001f79878412d90c7d3
Charlie Lindsay <charlieblindsay@gmail.com>
2024-06-28 13:31:32 +0100

    once again trying change in dockerfile

--- app/evaluation_tests.py
+++ app/evaluation_tests.py
@@ -7,7 +7,7 @@ import unittest
 from .utils.LLMCaller import GPT_3_point_5_turbo, ClaudeSonnetLLM
 from .evaluation import Params, evaluation_function
 from .example_risk_assessments import *
-from .utils.RegexPatternMatcher import RegexPatternMatcher
+from .RegexPatternMatcher import RegexPatternMatcher
 from .prompts.NoInformationProvided import NoInformationProvided
 from .prompts.HowItHarmsInContext import HowItHarmsInContext
 from .prompts.WhoItHarmsInContext import WhoItHarmsInContext
